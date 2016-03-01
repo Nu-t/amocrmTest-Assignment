c997e07438de51bd869f109ff416b6df6e27cff4
rtk-s <nikolay.malofeev@gmail.com>
2016-03-01 13:13:40 +0300

    Initial Commit

---
+++ amocrmTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  amocrmTest
+//
+//  Created by Nikolay on 01.03.16.
+//  Copyright © 2016 nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ amocrmTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  amocrmTest
+//
+//  Created by Nikolay on 01.03.16.
+//  Copyright © 2016 nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
